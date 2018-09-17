68740efe676418ff8315c3e8f0dbe497631944bf
Alejandro Huerta <ahuertam@yahoo.es>
2018-09-17 12:00:58 +0200

    No rivers and work

--- src/map-generator.ts
+++ src/map-generator.ts
@@ -22,8 +22,8 @@ function randomHeight(q: number, r: number) {
  */
 export async function generateMap(size: number, tile: (q: number, r: number) => TileData): Promise<Grid<TileData>> {
     const grid = new Grid<TileData>(size, size).mapQR((q, r) => tile(q, r))
-    const withRivers = generateRivers(grid)
-    return withRivers
+    // const withRivers = generateRivers(grid)
+    return grid
 }
 
 export async function generateRandomMap(size: number, tile: (q: number, r: number, height: Height) => TileData): Promise<Grid<TileData>> {
@@ -31,60 +31,60 @@ export async function generateRandomMap(size: number, tile: (q: number, r: numbe
     return generateMap(size, (q, r) => tile(q, r, randomHeight(q, r)))
 }
 
-function generateRivers(grid: Grid<TileData>): Grid<TileData> {
-    // find a few river spawn points, preferably in mountains
-    const tiles = grid.toArray()
-    const numRivers = Math.max(1, Math.round(Math.sqrt(grid.length) / 4))
-    const spawns: TileData[] = shuffle(tiles.filter(t => isAccessibleMountain(t, grid))).slice(0, numRivers)
-
-    // grow the river towards the water by following the height gradient
-    const rivers = spawns.map(growRiver)
-
-    // assign sequential indices to rivers and their tiles
-    rivers.forEach((river, riverIndex) => {
-        river.forEach((tile, riverTileIndex) => {
-            if (riverTileIndex < river.length - 1) {
-                tile.rivers = [{riverIndex, riverTileIndex}]
-            }
-        })
-    })
-
-    return grid
-
-    function growRiver(spawn: TileData): TileData[] {
-        const river = [spawn]
-
-        let tile = spawn
-
-        while (!isWater(tile.height) && river.length < 20) {
-            const neighbors = sortByHeight(grid.neighbors(tile.q, tile.r)).filter(t => !contains(t, river))
-            if (neighbors.length == 0) {
-                console.info("Aborted river generation", river, tile)
-                return river
-            }
-
-            const next = neighbors[Math.max(neighbors.length - 1, Math.floor(Math.random() * 1.2))]
-            river.push(next)
-
-            tile = next
-        }
-
-        return river
-    }
-
-    function sortByHeight(tiles: TileData[]): TileData[] {
-        return tiles.sort((a, b) => b.height - a.height)
-    }
-
-    function contains(t: TileData, ts: TileData[]) {
-        for (let other of ts) {
-            if (other.q == t.q && other.r == t.r) {
-                return true
-            }
-        }
-        return false
-    }
-}
+// function generateRivers(grid: Grid<TileData>): Grid<TileData> {
+//     // find a few river spawn points, preferably in mountains
+//     const tiles = grid.toArray()
+//     const numRivers = Math.max(1, Math.round(Math.sqrt(grid.length) / 4))
+//     const spawns: TileData[] = shuffle(tiles.filter(t => isAccessibleMountain(t, grid))).slice(0, numRivers)
+//
+//     // grow the river towards the water by following the height gradient
+//     const rivers = spawns.map(growRiver)
+//
+//     // assign sequential indices to rivers and their tiles
+//     rivers.forEach((river, riverIndex) => {
+//         river.forEach((tile, riverTileIndex) => {
+//             if (riverTileIndex < river.length - 1) {
+//                 tile.rivers = [{riverIndex, riverTileIndex}]
+//             }
+//         })
+//     })
+//
+//     return grid
+//
+//     function growRiver(spawn: TileData): TileData[] {
+//         const river = [spawn]
+//
+//         let tile = spawn
+//
+//         while (!isWater(tile.height) && river.length < 20) {
+//             const neighbors = sortByHeight(grid.neighbors(tile.q, tile.r)).filter(t => !contains(t, river))
+//             if (neighbors.length == 0) {
+//                 console.info("Aborted river generation", river, tile)
+//                 return river
+//             }
+//
+//             const next = neighbors[Math.max(neighbors.length - 1, Math.floor(Math.random() * 1.2))]
+//             river.push(next)
+//
+//             tile = next
+//         }
+//
+//         return river
+//     }
+//
+//     function sortByHeight(tiles: TileData[]): TileData[] {
+//         return tiles.sort((a, b) => b.height - a.height)
+//     }
+//
+//     function contains(t: TileData, ts: TileData[]) {
+//         for (let other of ts) {
+//             if (other.q == t.q && other.r == t.r) {
+//                 return true
+//             }
+//         }
+//         return false
+//     }
+// }
 
 function isAccessibleMountain(tile: TileData, grid: Grid<TileData>) {
     let ns = grid.neighbors(tile.q, tile.r)
